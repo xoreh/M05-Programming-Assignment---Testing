fa338d68097531957a259eb8a0d64be1299a72ee
xoreh <108965673+xoreh@users.noreply.github.com>
2022-11-24 00:08:43 -0500

    Update test.py

--- test.py
+++ test.py
@@ -27,5 +27,14 @@ class TestSum(unittest.TestCase):
         self.assertEqual(result, 1)
 
 if __name__ == '__main__':
-
     unittest.main()
+
+#I get this error-------->
+#Traceback (most recent call last):
+#  File "c:\Users\jorda\Documents\ACODE\IvyTechCode\M05 Programming Assignment - Testing\test.py", line 27, in test_list_fraction
+#    self.assertEqual(result, 1)
+#AssertionError: Fraction(9, 10) != 1
+
+#To me this error basically means that the sum of the fractions on test_list_fraction
+#more specifically on result, is not equals to 1 on self.assertEqual(result, 1)
+#for example when I run TestSum alone I do not get any erros, instead I get an OK message!
