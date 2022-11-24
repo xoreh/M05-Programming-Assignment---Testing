29232c48ad6acb62d196d628494a69be0d761132
xoreh <108965673+xoreh@users.noreply.github.com>
2022-11-24 00:10:58 -0500

    Update test_sum_unittest.py

--- test_sum_unittest.py
+++ test_sum_unittest.py
@@ -14,30 +14,29 @@ class TestSum(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
-#This is the following error when the file is run
+#######This is the following error when the file is run#######
 
 #Exception has occurred: SystemExit
 #True
 #  File "C:\Users\jorda\Documents\ACODE\IvyTechCode\M05 Programming Assignment - Testing\test_sum_unittest.py", line 13, in <module>
 #    unittest.main()
 
-#terminal
 #Ran 2 tests in 0.003s
 #
 #FAILED (failures=1)
 
 
+#######The following is going to be in the terminal#######
 
-
-#The nose part
+######The nose part######
 ######pip install nose2######
 #Installing collected packages: nose2
 #Successfully installed nose2-0.12.0
 
 ######python -m nose2######
 #PS C:\Users\jorda\Documents\ACODE\IvyTechCode\M05 Programming Assignment - Testing> python -m nose2
-#it gave me an error
 
+######it gave me an error######
 #..F
 #======================================================================
 #FAIL: test_sum_tuple (test_sum_unittest.TestSum.test_sum_tuple)       
@@ -54,8 +53,8 @@ if __name__ == '__main__':
 
 
 
-#The pytest part
-#nothing appears since everything is successful 
+######The pytest part######
+######nothing appears since everything is successful ######
 def test_sum():
     assert sum([1, 2, 3]) == 6, "Should be 6"
 
